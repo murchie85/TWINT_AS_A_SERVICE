df2ecfe1f649e6b73cb2d2d9500717078c92b5e3
Adam <murchie85@gmail.com>
2019-07-05 00:06:00 +0100

    more

--- inject_data.py
+++ inject_data.py
@@ -8,7 +8,7 @@ target = ''
 
 while True:
     try:
-        profiles = int(input('Welcome, how many profiles would you like to scan? *use numbers only please*'))
+        profiles = int(input('Welcome, how many profiles would you like to scan? ***use numbers only please***'))
         break
     except:
         print("That's not a valid option!")
@@ -19,11 +19,30 @@ for p in range(0,profiles) :
 	print('Please enterthe screen name of target i.e. realDonaldTrump - exclude the @ symbol')
 	target = input()
 	target = str(target)
-	print("Scan for {0}?".format(target) + '?\nY/N')
-	answer = input()
-	if answer == 'y' or answer == 'Y':
-		print('scanning')
-		subprocess.call(['bash','/Users/adammcmurchie/projects/docker/twint_as_a_service/injector.sh',target])
+	print('Scan Depth? A: 20 B: 60 C: 120 D: 200')
+	depth = input()
+	depth = str(depth)
+	if depth == 'A' or depth == 'a':
+		depth = '20'
+	elif depth == 'b' or depth == 'B':
+		depth = '60'
+	elif depth == 'c' or depth == 'C':
+		depth = '120'
+	elif depth == 'd' or depth == 'D':
+		depth = '200'
+	else:
+		print('No depth selected, defaulting to 20..')
+		depth = '20'
 
+	print('')
+	print("Scanning at depth{0}".format(depth))
+	print('')
+	print("Scan for {0}?".format(target) + '?\nY/N')
+	print('')
+	answer = input()
+	print('')
+	if answer =='Y' or answer =='y':
+		print('scanning.....')
+		subprocess.call(['bash','/Users/adammcmurchie/projects/docker/twint_as_a_service/injector.sh',target, depth])
 	else:
 		print('Skipping - did not enter y or Y')
--- injector.sh
+++ injector.sh
@@ -1,10 +1,18 @@
 
 echo 'the target is'
 echo $1
+echo 'the depth is'
+echo $2
 
-docker exec -it twint_app bash -c 'cd twint; exec twint -u '$1'  -es elasticsearch:9200 --limit 50'
-docker exec -it twint_app bash -c 'cd twint; exec twint -u '$1'  --following -es elasticsearch:9200 --limit 50'
-docker exec -it twint_app bash -c 'cd twint; exec twint -u '$1'  --favorites -es elasticsearch:9200 --limit 50 '
-docker exec -it twint_app bash -c 'cd twint; exec twint -u '$1'  --stats -es elasticsearch:9200 --limit 50'
-docker exec -it twint_app bash -c 'cd twint; exec twint -u '$1'  --stats --limit 50'
+echo 'Getting tweets..'
+docker exec -it twint_app bash -c 'cd twint; exec twint -u '$1'  -es elasticsearch:9200 --limit '$2''
+echo 'Getting followers..'
+docker exec -it twint_app bash -c 'cd twint; exec twint -u '$1'  --followers -es elasticsearch:9200 --limit '$2''
+echo 'getting followings..'
+docker exec -it twint_app bash -c 'cd twint; exec twint -u '$1'  --following -es elasticsearch:9200 --limit '$2''
+echo 'getting favourites..'
+docker exec -it twint_app bash -c 'cd twint; exec twint -u '$1'  --favorites -es elasticsearch:9200 --limit '$2' '
+echo 'getting stats..'
+docker exec -it twint_app bash -c 'cd twint; exec twint -u '$1'  --stats -es elasticsearch:9200 --limit '$2''
+
 
